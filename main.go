4b9e0cebccaf4c08d1d5f99f07145dd41c7777e6
muabu <Kobyto10072007@gmail.com>
2026-09-16 13:45:20 +0500

    add ReadMe file

--- main.go
+++ main.go
@@ -14,11 +14,7 @@ func main() {
 	http.HandleFunc("/thinkertoy", handlers.ThinkertoyHandler)
 	http.HandleFunc("/shadow", handlers.ShadowHandler)
 
-<<<<<<< HEAD
 	fmt.Println("Server started at localhost:9091")
-=======
-	fmt.Println("The server started at: localhost:8080")
->>>>>>> dc5fd21946c56b466a2f03fda0d1065f55bf512f
 	if err := http.ListenAndServe(":9091", nil); err != nil {
 		fmt.Println("Failed to run http server", err)
 	}
